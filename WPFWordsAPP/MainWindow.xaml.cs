f4048cb19a4a649d44c24c2f6f63fa9ca79b2855
Nestor <nestorkhanas@gmail.com>
2025-07-07 21:27:41 +0300

    Add some folders

--- WPFWordsAPP/MainWindow.xaml.cs
+++ WPFWordsAPP/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPFWordsAPP;
 
@@ -19,5 +10,14 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        string inputText = txtWord.Text;
+        if(inputText != "")
+        {
+            MessageBox.Show("You entered: " + inputText, "Input Confirmation");
+        }
+
     }
 }
